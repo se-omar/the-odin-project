d2c351404a71c514cf171260d11b54bc4d7c0896
Omar Ayman <se.omarayman@gmail.com>
2023-03-11 17:28:31 +0200

    finished messageBoard index page

--- node/projects/message-board/routes/index.js
+++ node/projects/message-board/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hello World!",
+    user: "Mohamed",
+    added: new Date(),
+  },
+  {
+    text: "Bye World",
+    user: "Omar",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Message Board", messages });
 });
 
 module.exports = router;
