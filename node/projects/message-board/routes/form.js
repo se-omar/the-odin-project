700a0da199e376b8c3e9827d7cbd2446580371e6
Omar Ayman <se.omarayman@gmail.com>
2023-03-11 22:33:32 +0200

    added new message route to messageBoard project

--- node/projects/message-board/routes/form.js
+++ node/projects/message-board/routes/form.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-/* GET users listing. */
 router.get("/", (req, res, next) => {
-  res.send("respond with a resource");
+  res.render("form");
 });
 
 module.exports = router;
--- node/projects/message-board/routes/index.js
+++ node/projects/message-board/routes/index.js
@@ -20,4 +20,15 @@ router.get("/", (req, res, next) => {
   res.render("index", { title: "Message Board", messages });
 });
 
+router.post("/new", (req, res, next) => {
+  if (req.body.name && req.body.message) {
+    messages.push({
+      user: req.body.name,
+      text: req.body.message,
+      added: new Date(),
+    });
+  }
+  res.redirect("/");
+});
+
 module.exports = router;
