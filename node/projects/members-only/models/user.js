a8be625fb1d91852d6e8e912ae91ee3353bac33c
Omar Ayman <se.omarayman@gmail.com>
2023-03-25 19:31:11 +0200

    implemented user model

---
+++ node/projects/members-only/models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { isEmail } from "validator";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+  email: { type: String, required: true, validate: [isEmail, "invalid email"] },
+  membership: {
+    type: String,
+    required: true,
+    enum: ["User", "Member", "Admin"],
+    default: "User",
+  },
+  birth_date: { type: Date, required: true },
+  password: { type: String, required: true },
+});
+
+UserSchema.virtual("full_name").get(function () {
+  return `${this.first_name} ${this.last_name}`;
+});
+
+export default mongoose.model("User", UserSchema);
